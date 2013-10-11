66e168a397ffecff6c8edef4c31482b9687c12ad
Akhilesh05 <akhile@dr.com>
2013-10-11 17:21:41 +0000

    First controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Shelly::Application.routes.draw do
+  get "welcome/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
