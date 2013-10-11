75d380b4aa69c64c8f259647fd8e401591f17fdf
Girish Gopaul <akhile@dr.com>
2013-10-11 10:52:18 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,9 +1,15 @@
 class LearnController < ApplicationController
+  
+  require 'net/http'
   skip_before_action :verify_authenticity_token, only: [:lesson3]
+  
   def lesson1
   end
 
   def lesson2
+    uri = URI "http://akh-rails.herokuapp.com/learn/lesson4"
+    res = Net::HTTP.post_form uri, :name => params[:name]
+    render text: "Hi " + res.body
   end
 
   def lesson3
