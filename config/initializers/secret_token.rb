85a6c839ce702eb43f052de644c23300d4af67f8
Akhilesh05 <akhile@dr.com>
2013-10-11 16:22:00 +0000

    78

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shelly::Application.config.secret_key_base = '59c207d690b45ea4d755231c77ab96a6a5defc15edb4279673b5a630dc0ab08a022c8b07ed2fefdea31d39b8e553b0dd9af0000da7b836e8b9f09677584cbd1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shelly::Application.config.session_store :cookie_store, key: '_shelly_session'
