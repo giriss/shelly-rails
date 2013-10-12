0350edd89b0cb36c27ec0d2bcba089c869e7393c
Girish Gopaul <akhile@dr.com>
2013-10-11 23:50:11 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -4,6 +4,16 @@ class LearnController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:lesson3]
   
   def lesson1
+    require 'uri'
+    require 'net/http'
+    require 'net/https'
+    
+    data = "USER=email%40example.com&PASSWORD=g1VPD20qBH3qHmzJ&AMOUNT=25&CURRENCY=USD&RECEIVEREMAIL=recipient%40example.com&SENDEREMAIL=myemail%40example.com&PURCHASETYPE=1&NOTE=This+is+a+test+transaction.&TESTMODE=1"
+    uri = URI.parse("https://api.payza.com/svc/api.svc/sendmoney")
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    req = https.post(uri.path, data)
+    render text: req.message
   end
 
   def lesson2
