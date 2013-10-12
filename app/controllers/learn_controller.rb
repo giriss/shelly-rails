5f897cce6eb63b3393261adf36b0939702df5390
Girish Gopaul <akhile@dr.com>
2013-10-12 00:18:28 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -6,7 +6,8 @@ class LearnController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:lesson3]
   
   def lesson1
-    @data = "USER=akhil05%40mail.com&PASSWORD=g1VPD20qBH3qHmzJ&AMOUNT=25&CURRENCY=USD&RECEIVEREMAIL=recipient%40example.com&SENDEREMAIL=akhil05%40mail.com&PURCHASETYPE=1&NOTE=This+is+a+test+transaction.&TESTMODE=1"
+    # POST a send money using Payza api (Testmode=TRUE)
+    @data = {:USER => "akhil05%40mail.com", :PASSWORD => "g1VPD20qBH3qHmzJ", :AMOUNT => "25", :CURRENCY => "USD", :RECEIVEREMAIL => "recipient%40example.com", :SENDEREMAIL => "akhil05%40mail.com", :PURCHASETYPE => "1", :NOTE => "This+is+a+test+transaction.", :TESTMODE => "1"}
     @uri = URI.parse("https://api.payza.com/svc/api.svc/sendmoney")
     @https = Net::HTTP.new(@uri.host,@uri.port)
     @https.use_ssl = true
