4abaacee9ebc7e25c8e317a113b59a0903cb4bb0
Girish Gopaul <akhile@dr.com>
2013-10-12 09:27:54 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -19,12 +19,16 @@ class LearnController < ApplicationController
       :TESTMODE => "1"
     }
     @url = "https://api.payza.com/svc/api.svc/sendmoney"
+    @uri = URI @url
+=begin
     @uri = URI.parse(@url)
     @https = Net::HTTP.new(@uri.host,@uri.port)
     @https.use_ssl = true
     @post = Net::HTTP::Post.new(@uri.path)
     @post.set_form_data(@data)
     @req = @https.request(@post)
+=end
+    @req = Net::HTTP.post_form @uri, @data
     @ret = "Post to send money using payza (Test mode)<br />" + @req.body
     render text: @ret
   end
@@ -35,19 +39,8 @@ class LearnController < ApplicationController
     else
       @name = 'default'
     end
-    @data = {
-      :USER => "akhil05@mail.com",
-      :PASSWORD => "vaHYkV4Mkwqv8dzF",
-      :AMOUNT => "25",
-      :CURRENCY => "USD",
-      :RECEIVEREMAIL => "client_1_akhil05@mail.com",
-      :SENDEREMAIL => "akhil05@mail.com",
-      :PURCHASETYPE => "1",
-      :NOTE => "This+is+a+test+transaction.",
-      :TESTMODE => "1"
-    }
-    @uri = URI "https://api.payza.com/svc/api.svc/sendmoney" # "http://akh-rails.herokuapp.com/learn/lesson3"
-    @res = Net::HTTP.post_form @uri, @data # :name => @name
+    @uri = URI "http://akh-rails.herokuapp.com/learn/lesson3"
+    @res = Net::HTTP.post_form @uri, :name => @name
     render text: "Hi " + @res.body
   end
 
