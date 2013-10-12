310c643aa58e5f7bb5e205e93c5d2c1ca0bd2a5b
Girish Gopaul <akhile@dr.com>
2013-10-12 00:10:29 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,19 +1,18 @@
 class LearnController < ApplicationController
-  
+
+  require 'uri'
   require 'net/http'
+  require 'net/https'
   skip_before_action :verify_authenticity_token, only: [:lesson3]
   
   def lesson1
-    require 'uri'
-    require 'net/http'
-    require 'net/https'
-    
-    data = "USER=akhil05%40mail.com&PASSWORD=g1VPD20qBH3qHmzJ&AMOUNT=25&CURRENCY=USD&RECEIVEREMAIL=recipient%40example.com&SENDEREMAIL=akhil05%40mail.com&PURCHASETYPE=1&NOTE=This+is+a+test+transaction.&TESTMODE=1"
-    uri = URI.parse("https://api.payza.com/svc/api.svc/sendmoney")
-    https = Net::HTTP.new(uri.host,uri.port)
-    https.use_ssl = true
-    req = https.post(uri.path, data)
-    render text: req.message + req.code + req.body
+    @data = "USER=akhil05%40mail.com&PASSWORD=g1VPD20qBH3qHmzJ&AMOUNT=25&CURRENCY=USD&RECEIVEREMAIL=recipient%40example.com&SENDEREMAIL=akhil05%40mail.com&PURCHASETYPE=1&NOTE=This+is+a+test+transaction.&TESTMODE=1"
+    @uri = URI.parse("https://api.payza.com/svc/api.svc/sendmoney")
+    @https = Net::HTTP.new(@uri.host,@uri.port)
+    @https.use_ssl = true
+    @req = @https.post(@uri.path, @data)
+    @ret = "Req.message: " + @req.message + "<br />Req.code: " + @req.code + "<br />Response: " + @req.body
+    render text: @ret
   end
 
   def lesson2
@@ -22,9 +21,9 @@ class LearnController < ApplicationController
     else
       @name = 'default'
     end
-    uri = URI "http://akh-rails.herokuapp.com/learn/lesson3"
-    res = Net::HTTP.post_form uri, :name => @name
-    render text: "Hi " + res.body
+    @uri = URI "http://akh-rails.herokuapp.com/learn/lesson3"
+    @res = Net::HTTP.post_form @uri, :name => @name
+    render text: "Hi " + @res.body
   end
 
   def lesson3
