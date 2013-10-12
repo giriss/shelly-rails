308e007ca012903ab62ba99c88b7a334c11a9b5d
Girish Gopaul <akhile@dr.com>
2013-10-12 05:41:57 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -7,15 +7,15 @@ class LearnController < ApplicationController
   
   def lesson1
     # POST a send money using Payza api (Testmode=TRUE)
-    @data = {:USER => "akhil05%40mail.com", :PASSWORD => "GYKDrxxSRLtYlonp", :AMOUNT => "25", :CURRENCY => "USD", :RECEIVEREMAIL => "client_1_akhil05%40mail.com", :SENDEREMAIL => "akhil05%40mail.com", :PURCHASETYPE => "1", :NOTE => "This+is+not+a+test+transaction.", :TESTMODE => "1"}
+    # @data = {:USER => "akhil05%40mail.com", :PASSWORD => "GYKDrxxSRLtYlonp", :AMOUNT => "25", :CURRENCY => "USD", :RECEIVEREMAIL => "client_1_akhil05%40mail.com", :SENDEREMAIL => "akhil05%40mail.com", :PURCHASETYPE => "1", :NOTE => "This+is+not+a+test+transaction.", :TESTMODE => "1"}
     @uri = URI.parse("https://api.payza.com/svc/api.svc/sendmoney")
     @https = Net::HTTP.new(@uri.host,@uri.port)
     @https.use_ssl = true
     @post = Net::HTTP::Post.new(@uri.path)
     @post.set_form_data(:USER => "akhil05@mail.com", :PASSWORD => "GYKDrxxSRLtYlonp", :AMOUNT => "25", :CURRENCY => "USD", :RECEIVEREMAIL => "client_1_akhil05@mail.com", :SENDEREMAIL => "akhil05@mail.com", :PURCHASETYPE => "1", :NOTE => "This+is+not+a+test+transaction.", :TESTMODE => "1")
     @req = @https.request(@post)
-    @ret = @req # "Req.message: " + @req.message + "<br />Req.code: " + @req.code + "<br />Response: " + @req.body
-    render text: @ret.body
+    @ret = @req.body # "Req.message: " + @req.message + "<br />Req.code: " + @req.code + "<br />Response: " + @req.body
+    render text: @ret
   end
 
   def lesson2
