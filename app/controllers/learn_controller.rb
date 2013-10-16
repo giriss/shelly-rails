dc697bd11ecc383b1c2384eb62f6264bb891d38c
Girish Gopaul <akhile@dr.com>
2013-10-16 07:54:28 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -94,4 +94,78 @@ class LearnController < ApplicationController
     render text: "Hi " + @res.body
   end
   
+  def lesson6
+    
+  end
+  
+  def lesson7
+    @data = {
+      :METHOD => "setExpressCheckout",
+      :VERSION => "90",
+      :USER => "akhil05_api1.mail.com",
+      :PWD => "1381743824",
+      :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
+      :PAYMENTREQUEST_0_AMT => "10",
+      :PAYMENTREQUEST_0_CURRENCYCODE => "USD",
+      :PAYMENTREQUEST_0_PAYMENTACTION => "SALE",
+      :returnUrl => "http://gagkas.tk/learn/lesson10",
+      :cancelUrl => "http://fb.com"
+    }
+    @url = "https://api-3t.sandbox.paypal.com/nvp"
+    @uri = URI @url
+#=begin
+    @uri = URI.parse @url
+    @https = Net::HTTP.new @uri.host, @uri.port
+    @https.use_ssl = true
+    @post = Net::HTTP::Post.new @uri.path
+    @post.set_form_data @data
+    @req = @https.start {|https| https.request @post}
+#=end
+#   @req = Net::HTTP.post_form @uri, @data
+    @token = @req.body.split('=')[1].split('&')[0]
+    redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=#{@token}"
+  end
+  
+  def lesson8
+    @token = params[:token]
+    @data = {
+      :METHOD => "GetExpressCheckoutDetails",
+      :VERSION => "90",
+      :USER => "akhil05_api1.mail.com",
+      :PWD => "1381743824",
+      :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
+      :TOKEN => @token
+    }
+    @url = "https://api-3t.sandbox.paypal.com/nvp"
+    @uri = URI @url
+    @uri = URI.parse @url
+    @https = Net::HTTP.new @uri.host, @uri.port
+    @https.use_ssl = true
+    @post = Net::HTTP::Post.new @uri.path
+    @post.set_form_data @data
+    @req = @https.start {|https| https.request @post}
+    @payerid = params[:PayerID] #@req.body.split("PAYERID=")[1].split("&")[0]
+    @data = {
+      :METHOD => "DoExpressCheckoutPayment",
+      :VERSION => "90",
+      :USER => "akhil05_api1.mail.com",
+      :PWD => "1381743824",
+      :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
+      :TOKEN => @token,
+      :PAYERID => @payerid,
+      :PAYMENTREQUEST_0_AMT => "10",
+      :PAYMENTREQUEST_0_CURRENCYCODE => "USD",
+      :PAYMENTREQUEST_0_PAYMENTACTION => "SALE"
+    }
+    @url = "https://api-3t.sandbox.paypal.com/nvp"
+    @uri = URI @url
+    @uri = URI.parse @url
+    @https = Net::HTTP.new @uri.host, @uri.port
+    @https.use_ssl = true
+    @post = Net::HTTP::Post.new @uri.path
+    @post.set_form_data @data
+    @req = @https.start {|https| https.request @post}
+    render text: @req.body + "<br />Payments done i guess :p"
+  end
+  
 end
