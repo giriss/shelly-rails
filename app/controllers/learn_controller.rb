b0de91649f5cd923d6971a3bd0e4ebd366ba693e
Girish Gopaul <akhile@dr.com>
2013-10-21 02:13:58 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -93,28 +93,39 @@ class LearnController < ApplicationController
   end
   
   def lesson7
-    @data = {
-      :METHOD => "setExpressCheckout",
-      :VERSION => "90",
-      :USER => "akhil05_api1.mail.com",
-      :PWD => "1381743824",
-      :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
-      :PAYMENTREQUEST_0_AMT => "10",
-      :PAYMENTREQUEST_0_CURRENCYCODE => "USD",
-      :PAYMENTREQUEST_0_PAYMENTACTION => "SALE",
-      :returnUrl => "http://hselihka.tk/learn/lesson8",
-      :cancelUrl => "http://hselihka.tk/learn/lesson6"
-    }
-    @url = "https://api-3t.sandbox.paypal.com/nvp"
-    @uri = URI @url
-    @uri = URI.parse @url
-    @https = Net::HTTP.new @uri.host, @uri.port
-    @https.use_ssl = true
-    @post = Net::HTTP::Post.new @uri.path
-    @post.set_form_data @data
-    @req = @https.start {|https| https.request @post}
-    @token = @req.body.split('TOKEN=')[1].split('&')[0]
+    @api = PayPal::SDK::Merchant::API.new
     
+    # Build request object
+    @set_express_checkout = @api.build_set_express_checkout({
+      :SetExpressCheckoutRequestDetails => {
+        :ReturnURL => "http://gagkas.tk/learn/lesson10",
+        :CancelURL => "http://gagkas.tk/learn/lesson8",
+        :PaymentDetails => [{
+          :OrderTotal => {
+            :currencyID => "USD",
+            :value => "12.0" },
+          :ItemTotal => {
+            :currencyID => "USD",
+            :value => "12" },
+          :PaymentDetailsItem => [{
+            :Name => "Deposit money",
+            :Quantity => 1,
+            :Amount => {
+              :currencyID => "USD",
+              :value => "12" },
+            :ItemCategory => "Digital" }],
+          :PaymentAction => "Sale" }] } })
+    
+    # Make API call & get response
+    @set_express_checkout_response = @api.set_express_checkout(@set_express_checkout)
+    
+    # Access Response
+    if @set_express_checkout_response.success?
+      @token = @set_express_checkout_response.Token
+    else
+      @errors = @set_express_checkout_response.Errors
+    end
+
     redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=#{@token}"
   end
   
