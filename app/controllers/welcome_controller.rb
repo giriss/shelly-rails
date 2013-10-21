f7643fc9bb65dc76bb6d176aa90cbebd9744e372
Girish Gopaul <akhile@dr.com>
2013-10-21 11:08:26 -0700

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,11 @@ class WelcomeController < ApplicationController
   def home
     @message = "Welcome dr."
   end
+  
+  def ssl
+    @ret = <<eof
+037663EB86F368ACDF991DDAC6BB4A5253E51147
+comodoca.com
+eof
+    render text: @ret
 end
