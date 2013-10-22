4e44e73fccc69c9b96efe50b39e243684241c4d4
Girish Gopaul <akhile@dr.com>
2013-10-22 03:47:59 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def need_ssl
+    redirect_to protocol: "https://" unless (request.ssl? || RAILS_ENV != "production")
+  end
 end
