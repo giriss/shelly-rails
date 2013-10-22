c0c1aa8d47040aa398d8eeb6f72d91e7ed4abf30
Girish Gopaul <akhile@dr.com>
2013-10-22 05:22:54 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def redirect_to_https
-    redirect_to protocol: "https://" unless (request.ssl? || request.local?)
+    redirect_to protocol: "https://" + request.host + relative_uri unless (request.ssl? || request.local?)
   end
 end
