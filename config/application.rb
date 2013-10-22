717983e20ee3b2e9120e5bd7d645f22b85ecf5c8
Girish Gopaul <akhile@dr.com>
2013-10-22 03:57:39 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Shelly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  def need_ssl
+    redirect_to protocol: "https://" unless (request.ssl? || RAILS_ENV != "production")
+  end
   end
 end
